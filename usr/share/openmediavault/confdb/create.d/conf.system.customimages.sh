37e3b652c642e277613140307191ca277e2715f0
Simone Karin Lehmann <skl@lisanet.de>
2026-03-08 15:41:34 +0100

    add plugin to system, add reset button, add folder browser, refactor sls

--- usr/share/openmediavault/confdb/create.d/conf.system.customimages.sh
+++ usr/share/openmediavault/confdb/create.d/conf.system.customimages.sh
@@ -6,11 +6,11 @@ set -e
 if ! omv_config_exists "/config/system/customimages"; then
   omv_config_add_node "/config/system" "customimages"
   omv_config_add_key "/config/system/customimages" "imagefolder" "/usr/share/openmediavault/customimages/default/"
-  omv_config_add_key "/config/system/customimages" "loginimage" "/usr/share/openmediavault/customimages/default/login.jpg"
+  omv_config_add_key "/config/system/customimages" "loginimage" "login.jpg"
   omv_config_add_key "/config/system/customimages" "blendmode" "hard-light"
-  omv_config_add_key "/config/system/customimages" "standbyimage" "/usr/share/openmediavault/customimages/default/standby.jpg"
+  omv_config_add_key "/config/system/customimages" "standbyimage" "standby.jpg"
   omv_config_add_key "/config/system/customimages" "standbyblendmode" "luminosity"
-  omv_config_add_key "/config/system/customimages" "shutdownimage" "/usr/share/openmediavault/customimages/default/shutdown.jpg"
+  omv_config_add_key "/config/system/customimages" "shutdownimage" "shutdown.jpg"
   omv_config_add_key "/config/system/customimages" "shutdownblendmode" "luminosity"
 fi
 
