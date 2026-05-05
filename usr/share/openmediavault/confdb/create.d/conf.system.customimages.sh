9e5c9f0b40b0760126501bc84ff18ef31d5d61a3
Simone Karin Lehmann <skl@lisanet.de>
2026-05-05 20:51:22 +0200

    migrate new variables

--- usr/share/openmediavault/confdb/create.d/conf.system.customimages.sh
+++ usr/share/openmediavault/confdb/create.d/conf.system.customimages.sh
@@ -9,6 +9,8 @@ if ! omv_config_exists "/config/system/customimages"; then
   omv_config_add_key "/config/system/customimages" "loginimage" "login.jpg"
   omv_config_add_key "/config/system/customimages" "standbyimage" "standby.jpg"
   omv_config_add_key "/config/system/customimages" "shutdownimage" "shutdown.jpg"
+  omv_config_add_key "/config/system/customimages" "lighttheme" "0"
+  omv_config_add_key "/config/system/customimages" "darktheme" "0"
 fi
 
 exit 0
