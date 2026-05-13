3f7db875899c367d4e37dfab238dfaec3221a200
Simone Karin Lehmann <skl@lisanet.de>
2026-05-13 21:41:39 +0200

    migrate from old default folder to new checkbox for wallpapers

--- usr/share/openmediavault/confdb/migrations.d/conf.system.customthemes_8.2.3-1.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.system.customthemes_8.2.3-1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 . /usr/share/openmediavault/scripts/helper-functions
@@ -7,10 +7,17 @@ SERVICE_XPATH_NAME="customthemes"
 SERVICE_XPATH="/config/system/${SERVICE_XPATH_NAME}"
 
 if ! omv_config_exists "${SERVICE_XPATH}/removecursor"; then
-  omv_config_add_key "${SERVICE_XPATH}" "removecursor" "0"
-  omv_config_add_key "${SERVICE_XPATH}" "wallpapers" "0"
+    omv_config_add_key "${SERVICE_XPATH}" "removecursor" "0"
+   omv_config_add_key "${SERVICE_XPATH}" "wallpapers" "0"
 
-  omv-salt stage run --quiet prepare
+   omv-salt stage run --quiet prepare
+fi
+
+f="$(omv_config_get /config/system/customthemes/imagefolder)"
+if [[ "$f" =~ "/customthemes/default/" ]]; then
+    omv_config_update "/config/system/customthemes/imagefolder" "/usr/share/openmediavault/customthemes/"
+else
+    omv_config_update "/config/system/customthemes/wallpapers" "1"
 fi
 
 exit 0
